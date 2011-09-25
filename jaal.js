1c73a57ac16b1e8e15202efce0969167fa13d1a8
Arjun Upadhyaya <arjunbgsu@gmail.com>
2011-09-25 18:21:27 -0300

    Edited jaal.js via GitHub

--- jaal.js
+++ jaal.js
@@ -1,3 +1,25 @@
+/*
+Copyright (c) 2011 Arjun Upadhyaya <arjun@arjunupadhyaya.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 /*********************************************************************
 Javascript Asynchronous Anchor image loader ( background image loader)
 Version 0.1: Arjun Upadhyaya
@@ -148,7 +170,6 @@ Based on the excellent jquery plugin for lazy loading (jail.js) by Sebastiano Ar
 		later : function(options) {
 			var bckImages = this;
 			
-			//console.log(bckImages);
 			// If the 'load' event is specified, immediately load all the visible images and remove them from the stack
 		/*	if (options.event === 'load') {
 				images.each(function(){
